2214717fd1a41d9c16602b61acf0d29a83910533
nicolepng <56541613+nicolepng@users.noreply.github.com>
2020-03-24 14:40:36 +0800

    edits

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,15 @@
 import firebase from 'firebase';
 
 const firebaseConfig = {
-    //Key in your database details
-    apiKey: "",
-    authDomain: "",
-    databaseURL: "",
-    projectId: ""
-
-  };
+  apiKey: "AIzaSyCwDI5j0fkUwnx-VoOJR6DWglKbhBEugIY",
+  authDomain: "discovery-16619.firebaseapp.com",
+  databaseURL: "https://discovery-16619.firebaseio.com",
+  projectId: "discovery-16619",
+  storageBucket: "discovery-16619.appspot.com",
+  messagingSenderId: "553262723938",
+  appId: "1:553262723938:web:5a9c61013a7bd7c322b23b",
+  measurementId: "G-904LTFP4HW"
+};
   
   firebase.initializeApp(firebaseConfig);
   var database = firebase.firestore();
