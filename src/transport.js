ac05dc81c100b32cbdffe4891b58bc65f21bd754
nicolepng <56541613+nicolepng@users.noreply.github.com>
2020-03-29 15:00:04 +0800

    update graph

--- src/transport.js
+++ src/transport.js
@@ -10,39 +10,66 @@ export default{
           labels:[],
           datasets: [
             {
-              label: 'transport in singapore',
+              label: 'MRT',
                data:[],
               //backgroundColor:['aqua','lightgreen','red','orange'],
               borderWidth:0.5,
               borderColor:"orange",
               backgroundColor:'orange',
               fill:false
-            }
+            },
+            {
+              label: 'LRT',
+              data: [],
+              borderWidth: 0.5,
+              borderColor: "red",
+              backgroundColor: "red",
+              fill: false
+            },
+            {
+              label: "Bus",
+              data:[],
+              borderWidth:0.5,
+              borderColor:"blue",
+              backgroundColor:"blue",
+              fill: false
+            },
+            {
+              label: "Taxi",
+              data:[],
+              borderWidth:0.5,
+              borderColor:"green",
+              backgroundColor:"green",
+              fill: false
+            },
           ]
           
         },
         options: {
-           
-            scales:{
-                yAxes:[{
-                    ticks:{
-                        min:0
-                    }
-
-                }]
-            }
+          responsive: true,
         }
       }),
     methods:{
     
     fetchData : function(){
         axios.get('https://data.gov.sg/api/action/datastore_search?resource_id=552b8662-3cbc-48c0-9fbb-abdc07fb377a').then(response=>{
-        this.results=response.data.result.records[0]
+        this.results=response.data.result.records
         //console.log(response.data)
         //console.log(this.results)
         for(let key in this.results){
-            this.chartdata.datasets[0].data.push(this.results[key])
-            this.chartdata.labels.push(key+'')    
+          if (this.results[key].type_of_public_transport == "MRT"){
+            this.chartdata.datasets[0].data.push(this.results[key].average_ridership)
+            this.chartdata.labels.push(this.results[key].year+'') 
+          }
+          if (this.results[key].type_of_public_transport == "LRT"){
+            this.chartdata.datasets[1].data.push(this.results[key].average_ridership)
+          } 
+          if (this.results[key].type_of_public_transport == "Bus"){
+            this.chartdata.datasets[2].data.push(this.results[key].average_ridership)
+          }        
+          if (this.results[key].type_of_public_transport == "Taxi"){
+            this.chartdata.datasets[3].data.push(this.results[key].average_ridership)
+          }   
         }
         this.renderChart(this.chartdata,this.options)
             
